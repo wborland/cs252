aa7a7568bd61c0cc12a4c740bda89e0a131a0fbf
Will Borland <wborland@purdue.edu>
2018-04-10 00:02:32 -0400

    Added 404 page and light/dark theme

--- flaskapp.py
+++ flaskapp.py
@@ -5,18 +5,21 @@ app.config.update(
 	TEMPLATES_AUTO_RELOAD = True
 )
 
-@app.route('/')
+@app.route('/dark')
 def index():
-	return render_template('index.html')
+	return render_template('index-dark.html')
 
-@app.route('/gusty/<name>')
-def gusty(name):
-	return render_template('252.html', user=name)
+@app.route('/')
+def light():
+	return render_template('index-light.html')
 
 @app.route('/hey/me')
 def he():
         return "Hey"
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 
 if __name__ == '__main__':
 	app.run()
