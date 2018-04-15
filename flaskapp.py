201b777eb868212d362388d71821ca459817b7bb
Cameron <camjrichards6987@gmail.com>
2018-04-15 16:54:49 -0400

    Login working with session variables. Posting a ticket form done, script not working

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, send_from_directory, flash, redirect
-from forms import LoginForm
+from flask import Flask, render_template, send_from_directory, flash, redirect, session
+from forms import LoginForm, PostForm
+from flask_login import current_user, login_user
 import os
 
 import db
@@ -25,14 +26,48 @@ def index():
 def light():
 	return render_template('index-light.html')
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/logout')
+def logout():
+        session.pop('username', None)
+        return render_template('/')
+
+@app.route('/login')
 def login():
+        #if current_user.is_authrnticated:
+            #flash('You are already logged in')
+            #return redirect('/')
+
+        form = LoginForm()
+        return render_template('login.html', form=form)
+
+@app.route('/login', methods=['POST'])
+def login2():
         form = LoginForm()
         if form.validate_on_submit():
             flash('Logged in as {} with password {}'.format(
                 form.email.data, form.password.data))
+            session['username'] = form.email.data
             return redirect('/')
-        return render_template('login.html', form=form)
+        else :
+            flash('You must fill out both fields')
+            return render_template('login.html', form=form)
+
+@app.route('/postTix')
+def postTix():
+        form = PostForm()
+        return render_template('postTicket.html', form=form)
+
+@app.route('/postTix', methods=['POST'])
+def postTix2():
+        form = PostForm()
+        if form.validate_on_submit(): 
+            flash('Post for event {} on date {} at {} for price {}'.format(
+                form.event.data, form.time.data, form.location.data, form.price.data))
+            return redirect('/')
+        else :
+            flash('That was not valid')
+            return render_template('postTicket.html', form=form)
+
 
 @app.route('/hey/me')
 def he():
@@ -43,4 +78,6 @@ def page_not_found(e):
     return render_template('404.html')
 
 if __name__ == '__main__':
-	app.run()
+        app.secret_key = 'dvaz'
+        app.run()
+
--- forms.py
+++ forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, DecimalField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+class PostForm(FlaskForm):
+    event = StringField('Event Name', validators=[DataRequired()])
+    time = DateTimeField('Date and Time', validators=[DataRequired()])
+    location = StringField('Event Location', validators=[DataRequired()])
+    price = DecimalField('Ticket Price', validators=[DataRequired()], places=2)
+    comments = StringField('Comments')
+    submit = SubmitField('Post Ticket For Sale')
+
