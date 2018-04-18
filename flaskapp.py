f5f4bb00bcfafc0d3ebc38a98f510cf7e3703cbc
Cameron <camjrichards6987@gmail.com>
2018-04-17 20:57:40 -0400

    Can register a new user client side

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory, flash, redirect, session
-from forms import LoginForm, PostForm
+from forms import LoginForm, PostForm, RegisterForm
 from flask_login import current_user, login_user
 import os
 
@@ -33,17 +33,13 @@ def logout():
 
 @app.route('/login')
 def login():
-        #if current_user.is_authrnticated:
-            #flash('You are already logged in')
-            #return redirect('/')
-
         form = LoginForm()
         return render_template('login.html', form=form)
 
 @app.route('/login', methods=['POST'])
 def login2():
         form = LoginForm()
-        if form.validate_on_submit():
+        if form.validate_on_submit(): #and user exists in db
             flash('Logged in as {} with password {}'.format(
                 form.email.data, form.password.data))
             session['username'] = form.email.data
@@ -51,6 +47,36 @@ def login2():
         else :
             flash('You must fill out both fields')
             return render_template('login.html', form=form)
+
+@app.route('/register')
+def register():
+        form = RegisterForm()
+        return render_template('register.html', form=form)
+
+@app.route('/register', methods=['POST'])
+def register2():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.confirmPass.data:
+            flash('Passwords must match')
+            return render_template('register.html', form=form)
+
+        if form.venmo.data == False and form.facebook.data == False and form.apple.data == False and form.android.data == False and form.dollars.data == False:
+            flash('You must select at least one form of payment')
+            return render_template('register.html', form=form)
+
+        methods = ''
+        for field in form:
+            if field.type == "BooleanField" and field.data == True:
+                methods += field.label.text
+                methods += ','
+        flash('selected methods: ' + methods)
+        session['username'] = form.email.data
+        flash('logged in as: ' + form.email.data)
+        return redirect('/')
+    else:
+        flash('Please fill out every field')
+        return render_template('register.html', form=form)
 
 @app.route('/postTix')
 def postTix():
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, DecimalField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, DateTimeField, DecimalField, BooleanField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
@@ -11,7 +11,20 @@ class PostForm(FlaskForm):
     event = StringField('Event Name', validators=[DataRequired()])
     time = DateTimeField('Date and Time', validators=[DataRequired()])
     location = StringField('Event Location', validators=[DataRequired()])
-    price = DecimalField('Ticket Price', validators=[DataRequired()], places=2)
+    price = DecimalField('Ticket Price (US $)', validators=[DataRequired()], places=2)
     comments = StringField('Comments')
     submit = SubmitField('Post Ticket For Sale')
 
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    firstName = StringField('First Name', validators=[DataRequired()])
+    lastName = StringField('Last Name', validators=[DataRequired()])
+    venmo = BooleanField('Venmo')
+    facebook = BooleanField('Facebook')
+    apple = BooleanField('Apple Pay')
+    android = BooleanField('Android Pay')
+    dollars = BooleanField('Cash')
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPass = PasswordField('Retype Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+    
